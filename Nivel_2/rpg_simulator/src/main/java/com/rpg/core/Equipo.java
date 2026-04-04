23765b01d38673e44adb73275b8db82b85f61729
StalkerData <marlon.duquemar@unipamplona.edu.co>
2026-04-04 16:26:21 -0500

    Se empezó trabajar con la clase Equipos, se hizo los métodos constructores, agregarMiembro y tieneVivos, se creal la excepción LimiteEquipoSuperado.

--- Nivel_2/rpg_simulator/src/main/java/com/rpg/core/Equipo.java
+++ Nivel_2/rpg_simulator/src/main/java/com/rpg/core/Equipo.java
@@ -1,5 +1,67 @@
 package main.java.com.rpg.core;
 
-public class Equipo {
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.com.rpg.exception.LimiteEquipoSuperado;
+import main.java.com.rpg.models.Personaje;
+
+public class Equipo<T extends Personaje> {
+	private final int limite = 3;
+	private String nombreEquipo;
+	private List<T> miembros;
+
+	public Equipo(String nombreEquipo) {
+		super();
+		this.nombreEquipo = nombreEquipo;
+		this.miembros = new ArrayList<T>();
+	}
+
+	public Equipo(String nombreEquipo, List<T> miembros) throws LimiteEquipoSuperado {
+		super();
+		this.nombreEquipo = nombreEquipo;
+
+		if (miembros.size() > limite) {
+			throw new LimiteEquipoSuperado("El equipo supera el tamano permitido del Party.");
+		}
+		this.miembros = miembros;
+
+	}
+
+	public void agregarMiembro(T personaje) throws LimiteEquipoSuperado {
+		if (miembros.size() > limite) {
+			throw new LimiteEquipoSuperado("No e puede agregar este personaje, supera el limite del Party.");
+		}
+		miembros.add(personaje);
+	}
+
+	public boolean tieneVivos() {
+		for (T t : miembros) {
+			if (t.isEstaVivo() == true)
+				return true;
+		}
+		return false;
+	}
+
+	public T obtenerPersonajeVivoAleatorio() {
+		return null;
+
+	}
+
+	public int getLimite() {
+		return limite;
+	}
+
+	public String getNombreEquipo() {
+
+		return nombreEquipo;
+	}
+
+	public List<T> getMiembros() {
+		return List.copyOf(miembros);
+		// miembros.stream().collect(Collectors.toList());
+		// new ArrayList<>(miembros);
+		// new ArrayList<>().addAll(miembros);
+	}
 
 }
---
+++ Nivel_2/rpg_simulator/src/main/java/com/rpg/exception/LimiteEquipoSuperado.java
@@ -0,0 +1,11 @@
+package main.java.com.rpg.exception;
+
+public class LimiteEquipoSuperado extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public LimiteEquipoSuperado(String msg) {
+		super(msg);
+	}
+
+}
