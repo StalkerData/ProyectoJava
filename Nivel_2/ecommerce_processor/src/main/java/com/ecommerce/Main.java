188ab96c438947bf259aa803f2247bb3e23de06b
StalkerData <marlon.duquemar@unipamplona.edu.co>
2026-06-06 10:24:26 -0500

    Se comenzó con las pruebas se hicieron pruebas a los métodos calcularTotalSinImpuestos y calcularTotalConImpuestos.

--- Nivel_2/ecommerce_processor/src/main/java/com/ecommerce/Main.java
+++ Nivel_2/ecommerce_processor/src/main/java/com/ecommerce/Main.java
@@ -28,30 +28,35 @@ public class Main {
 		for (Producto producto : ProcesadorDePedidos.filtrarPorCategoria(pedido1, Categoria.ELECTRONICA)) {
 			System.out.println(producto);
 		}
-		
+
 		System.out.println("\nSin Ordenar");
 		for (Producto producto : pedido1.getCarrito()) {
 			System.out.println(producto);
 		}
-		
+
 		System.out.println("\nOrdenado por presio");
 		ProcesadorDePedidos.ordenarProductosPorPrecio(pedido1, Comparator.comparing(Producto::precio).reversed());
 		for (Producto producto : pedido1.getCarrito()) {
 			System.out.println(producto);
 		}
-		
+
 		try {
-		    ProcesadorDePedidos.procesar(
-		        pedido1,
-		        new PagoCripto("1211aeff3c", 8000.0)
-		    );
+			ProcesadorDePedidos.procesar(pedido1, new PagoCripto("1211aeff3c", 8000.0));
 		} catch (FondosInsuficientesException e) {
-		    System.err.println("Error: Fondos insuficientes. Detalle: " + e.getMessage());
+			System.err.println("Error: Fondos insuficientes. Detalle: " + e.getMessage());
 		} catch (PagoFallidoException e) {
-		    System.err.println("Error: Pago fallido. Detalle: " + e.getMessage());
+			System.err.println("Error: Pago fallido. Detalle: " + e.getMessage());
 		} catch (Exception e) {
-		    System.err.println("Error inesperado al procesar el pago: " + e.getMessage());
-		    e.printStackTrace(); 
+			System.err.println("Error inesperado al procesar el pago: " + e.getMessage());
+			e.printStackTrace();
 		}
+
+		p1 = new Producto("001", "Teclado", 100.0, Categoria.ELECTRONICA);
+		p2 = new Producto("002", "Camisa", 50.0, Categoria.ROPA);
+		p3 = new Producto("003", "Manzana", 2.0, Categoria.ALIMENTOS);
+		productoList = List.of(p1, p2, p3);
+		pedido1 = new Pedido(productoList);
+		System.out.println(ProcesadorDePedidos.calcularTotalSinImpuestos(pedido1));
+		System.out.println(ProcesadorDePedidos.calcularTotalConImpuestos(pedido1));
 	}
 }
---
+++ Nivel_2/ecommerce_processor/src/test/java/com/ecommerce/ProcesadorDePedidosTest.java
@@ -0,0 +1,44 @@
+package test.java.com.ecommerce;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import main.java.com.ecommerce.model.Categoria;
+import main.java.com.ecommerce.model.Pedido;
+import main.java.com.ecommerce.model.Producto;
+import main.java.com.ecommerce.service.ProcesadorDePedidos;
+
+class ProcesadorDePedidosTest {
+	private Pedido testPedido;
+	private List<Producto> listProductos1;
+	private List<Producto> listProductos2;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Producto p1 = new Producto("001", "Teclado", 100.0, Categoria.ELECTRONICA);
+		Producto p2 = new Producto("002", "Camisa", 50.0, Categoria.ROPA);
+		Producto p3 = new Producto("003", "Manzana", 2.0, Categoria.ALIMENTOS);
+		Producto p4 = new Producto("004", "Laptop", 999.99, Categoria.ELECTRONICA);
+		Producto p5 = new Producto("005", "Falda", 30.0, Categoria.ROPA);
+		Producto p6 = new Producto("006", "Vestido", 50.0, Categoria.ROPA);
+		listProductos1 = List.of(p1, p2, p3);
+		listProductos2 = List.of(p1, p4, p2, p5, p6);
+	}
+
+	@Test
+	void tesTotalSinImpuestos() {
+		testPedido = new Pedido(listProductos1);
+		assertEquals(152.0, ProcesadorDePedidos.calcularTotalSinImpuestos(testPedido));
+	}
+	
+	@Test
+	void tesTotalConImpuestos() {
+		testPedido = new Pedido(listProductos1);
+		assertEquals(177.1, ProcesadorDePedidos.calcularTotalConImpuestos(testPedido), 0.01);
+	}
+
+}
